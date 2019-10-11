9b94201aaa9ae2e6e8560959edd44bbca3ca6528
Travis Geisler <tgeis98@gmail.com>
2019-10-11 00:53:35 -0500

    Finished version of loan calculator

--- app.py
+++ app.py
@@ -1,16 +1,27 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, request, redirect
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html', pageTitle='Flask Server Home Page')
+    return render_template('index.html', display="", pageTitle='My Calculator')
 
-@app.route('/travis')
-def travis():
-    return render_template('travis.html', pageTitle='About Travis')
-
+@app.route('/loan', methods=['GET', 'POST'])
+def loan():
+   if request.method == 'POST':
+        form = request.form
+        amount = float(form['amount'])
+        nperyear= float(form['nperyear'])
+        years= float(form['years'])
+        rate= float(form['rate'])
+        n = nperyear * years
+        i = rate / nperyear
+        D = ((( 1 + i ) ** n ) - 1 ) / ( i * ( 1 + i) ** n )
+        calc = "Your loan payment is $" + str(round(amount / D, 2))
+        return render_template('index.html', display=calc, pageTitle='My Calculator')
+    
+     
 
 if __name__ == '__main__':
     app.run(debug=True)
